d54b1e46fb166107b79c4419dd66075d41a9160e
Orest Kostiuk <xxmolekula@gmail.com>
2017-10-02 19:27:00 +0300

    test all controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,10 @@
 class QuestionsController < ApplicationController
-  #before_action :question_params, only: [ :show, :destroy, :update, :edit]
+  before_action :question_params, only: [ :show, :edit, :update, :destroy]
   def index
    @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
   end
 
   def new
@@ -13,13 +12,36 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find(params[:id])
+  end
+
+  def create
+    @question = Question.create(set_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def update
+    @question.update(set_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to question_path
+
   end
   private
   def question_params
     @question = Question.find(params[:id])
   end
-  def question_params
-    #params.require(:question).permit(:title, :body)
+  def set_params
+    params.require(:question).permit(:title, :body)
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -41,4 +41,65 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :edit
     end
   end
+  describe "POST #create" do
+    context "with valid" do
+      it "save new question!" do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+      it "redirect to show view" do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+    context "with not valid" do
+      it "not save!" do
+        expect { post :create, params: { question: attributes_for(:invalid_question) } }.to_not change(Question, :count)
+      end
+      it "re-render new" do
+        post :create, params: { question: attributes_for(:invalid_question) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+  describe "PATCH #update" do
+    context "valid attributes" do
+      it "assigns the request question to @question" do
+        patch :update, params: { id: question.id, question: attributes_for(:question) }
+        #expect(assigns(:question)).to eq question
+      end
+      it "changes question attributes" do
+        patch :update, params: { id: question.id , question: {title: "newd", body: "newq"} }
+        question.reload
+        expect(question.title).to eq "newd"
+        expect(question.body).to eq "newq"
+      end
+      it "redirect to the update question" do
+        patch :update, params: { id: question.id, question: attributes_for(:question) }
+        expect(response).to redirect_to question
+      end
+    end
+    context "invelid attributes" do
+      before { patch :update, params: { id: question.id , question: {title: nil, body: "newq"} }  }
+      it "not update" do
+        question.reload
+        expect(question.title).to eq "MyString"
+        expect(question.body).to eq "MyText"
+      end
+      it "re-render edit" do
+        expect(response).to render_template :edit
+      end
+    end
+
+  end
+  describe "DELETE #destroy" do
+    it "delete question" do
+      question
+      expect{ delete :destroy, params: { id: question.id } }.to change(Question, :count).by(-1)
+    end
+    it "redirect to index view" do
+      delete :destroy, params: { id: question.id }
+      expect(response).to redirect_to question_path
+    end
+  end
+
 end
