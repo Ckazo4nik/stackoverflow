fab14f05c915054a1f0c60a16c2c5e0e4cccf34f
Orest Kostiuk <xxmolekula@gmail.com>
2017-10-11 22:39:16 +0300

    Поліморфічний звязок.Відповідь має прикріплені файли

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,5 +3,6 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
 
-
+  has_many :attachments, as: :attachmentable
+  accepts_nested_attributes_for :attachments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011182220) do
+ActiveRecord::Schema.define(version: 20171011193112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,10 @@ ActiveRecord::Schema.define(version: 20171011182220) do
     t.string "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "question_id"
-    t.index ["question_id"], name: "index_attachments_on_question_id"
+    t.integer "attachmentable_id"
+    t.string "attachmentable_type"
+    t.index ["attachmentable_id"], name: "index_attachments_on_attachmentable_id"
+    t.index ["attachmentable_type"], name: "index_attachments_on_attachmentable_type"
   end
 
   create_table "questions", force: :cascade do |t|
